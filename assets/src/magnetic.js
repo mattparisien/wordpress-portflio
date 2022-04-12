030393e1e77c0edf2c07644988ab5d4a9eb37e9e
Matt Parisien <matthewparisien4@gmail.com>
2022-04-12 19:19:01 -0400

    Develop magnetic class

--- assets/src/magnetic.js
+++ assets/src/magnetic.js
@@ -12,6 +12,51 @@ export default class MagneticItem {
 		this.easing = {
 			x: 0,
 			y: 0,
+			scale: 1,
+			value: this.target.getAttribute("data-easing"),
 		};
+		this.transform = {
+			x: 0,
+			y: 0,
+			scale: 1,
+			max: this.target.getAttribute("data-max"),
+		};
+		this.window = {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		};
+		(this.history = false),
+			(this.scale = this.target.getAttribute("data-scale"));
+	}
+
+	handleMouseMove(e) {
+		(this.mousePos.x = e.pageX), (this.mousePos.y = e.pageY);
+	}
+
+	handleResize(e) {
+		this.bounds = this.target.getBoundingClientRect();
+		this.window.width = window.innerWidth;
+		this.window.height = window.innerHeight;
+	}
+
+	makeMagnetic(x, y) {
+		const centerX = this.bounds.left + this.bounds.width / 2;
+		const centerY = this.bounds.top + this.bounds.height / 2;
+
+		const a = Math.abs(centerX - x);
+		const b = Math.abs(centerY - y);
+		const c = Math.sqrt(a * a + b * b);
+
+		const isHover = c < this.bounds.width / 2 + this.threshold;
+
+		if (!this.history && isHover) {
+			this.target.classList.add("is-hover");
+			this.threshold = this.threshold * this.ratio;
+			this.history = true;
+		} else if (this.history && !isHover) {
+			this.target.classList.remove("is-hover");
+			this.threshold = this.threshold / this.ratio;
+			this.history = false;
+		}
 	}
 }
